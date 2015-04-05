3ff29a14bbe5d30f10871652969fda6e1401b8a9
Matt R. <mrobold@gmail.com>
2015-04-04 22:46:41 -0700

    Colons
    It's like that scene in Real Genius: "The colon...what does it do?"

--- blinky.py
+++ blinky.py
@@ -7,7 +7,7 @@ def setup():
 	GPIO.setup(12, GPIO.OUT)
 
 def Blink(pin, iterations, speed):
-	for i in range(0, iterations)
+	for i in range(0, iterations):
 		GPIO.output(pin, True)
 		time.sleep(speed)
 		GPIO.output(pin, False)
