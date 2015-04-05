478fe423076cd418ef1e61afe6b5592d10620dc4
Matt R. <mrobold@gmail.com>
2015-04-04 22:34:16 -0700

    Seeing if I understand what I'm doing here

---
+++ blinky.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+def setup()
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(24, GPIO.OUT)
+	GPIO.setup(12, GPIO.OUT)
+
+def Blink(pin, iterations, speed)
+	for i in range(0, iterations)
+		GPIO.output(pin, True)
+		time.sleep(speed)
+		GPIO.output(pin, False)
+		time.sleep(speed)
+
+redIterations = 10
+redSpeed = 100
+redPin = 24
+
+yellowPin = 12
+
+setup()
+Blink(redPin, redIterations, redSpeed)
+
+GPIO.cleanup()
